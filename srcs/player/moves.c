86f83f9f683909e50b506fa2d1a75dd272873bb2
Luara Raggio <luararaggio@hotmail.com>
2025-03-18 18:54:18 -0300

    fix(player moves): try to fix player s move to the right

--- srcs/player/moves.c
+++ srcs/player/moves.c
@@ -6,7 +6,7 @@
 /*   By: lraggio <lraggio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 16:53:53 by lraggio           #+#    #+#             */
-/*   Updated: 2025/02/26 01:17:43 by lraggio          ###   ########.fr       */
+/*   Updated: 2025/03/18 18:51:15 by lraggio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,12 +50,12 @@ void	move_player_to_left(t_game *game, t_player *player)
 
 void	move_player_to_right(t_game *game, t_player *player)
 {
+	if (game->map->map_int[(int)(player->x)][(int)(player->y - player->plane_y
+			* player->move_speed)] == 0)
+		player->y -= player->plane_y * player->move_speed;
 	if (game->map->map_int[(int)(player->x - player->plane_x
 			* player->move_speed)][(int)(player->y)] == 0)
 		player->x -= player->plane_x * player->move_speed;
-	if (game->map->map_int[(int)(player->x)][(int)(player->y -
-			player->plane_y * player->move_speed)] == 0)
-		player->y -= player->plane_y * player->move_speed;
 	game->map->player_j = (int)player->x;
 	game->map->player_i = (int)player->y;
 }
